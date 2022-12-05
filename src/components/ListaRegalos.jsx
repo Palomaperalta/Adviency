8667832a2b7ef8eba2d24bc406732fe8929787da
Paloma Peralta <palomaperalta9@gmail.com>
2022-12-05 19:09:40 -0300

    dia 3 adviency

--- src/components/ListaRegalos.jsx
+++ src/components/ListaRegalos.jsx
@@ -3,11 +3,21 @@ import './ListaRegalos.css'
 
 
 function ListaRegalos() {
+const [nuevoRegalo, setNuevoRegalo] = useState('')
 const [regalos, setRegalos] = useState(['Caramelos', 'Medias', 'Vitel Tone'])
 
+
+  const handleOnClick = () => {
+    setRegalos([...regalos, nuevoRegalo])
+    setNuevoRegalo('')
+  }
   return (
-    <div>
+    <div className='container'>
       <h1>Regalos:</h1>
+      <div className='formulario'>
+        <input value={nuevoRegalo} onChange={(e)=>setNuevoRegalo(e.target.value)} type="text"/>
+        <button onClick={handleOnClick}>Agregar</button>
+      </div>
       <div className="listaregalos" >
         <ul className="regalos">
         {regalos.map(regalo =>{
