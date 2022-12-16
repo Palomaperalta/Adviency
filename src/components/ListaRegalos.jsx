9f0397d9bcd141eb951f16de8b13900f3680d720
Paloma Peralta <palomaperalta9@gmail.com>
2022-12-16 18:37:02 -0300

    dia 9, 10, 11, 12 y 13

--- src/components/ListaRegalos.jsx
+++ src/components/ListaRegalos.jsx
@@ -1,37 +1,109 @@
-import { useState } from 'react';
-import './ListaRegalos.css'
+import { useState, useEffect } from 'react';
+import './ListaRegalos.css';
+import Modal from 'react-modal'
 
 
 function ListaRegalos() {
-const [nuevoRegalo, setNuevoRegalo] = useState('')
-const [regalos, setRegalos] = useState([{name:'Caramelos', id: 0}, {name:'Medias',id: 1}, {name:'Vitel Tone', id:2}])
+  const [nuevoRegalo, setNuevoRegalo] = useState('')
+  const [regalos, setRegalos] = useState(() => {
+    const regalosguardados = JSON.parse(localStorage.getItem("regalos"));
+      if (regalosguardados) {
+        return regalosguardados
+      } else {
+        return []
+      }
+  })
+  const [cantidadregalos, setCantidadRegalos] = useState(0)
+  const [imagenRegalo, setImagenRegalo] = useState('')
+  const [openModal, setOpenModal] = useState(false)
+  const [destinatario, setDestinatario] = useState('')
+  const [editarRegaloId, setEditarRegaloId] = useState(null)
 
-  const handleOnClick = () => {
-    if(nuevoRegalo !== '' && !regalos.some((regalo) =>  regalo.name.toLowerCase() === nuevoRegalo.toLowerCase())){
-      setRegalos([...regalos, {name: nuevoRegalo, id: regalos.length}])
-      setNuevoRegalo('')
-    }    
-  }
   const handleDelete = (id) =>{
-    console.log(id)
     const regalosNoBorrados = regalos.filter(regalo=> id !== regalo.id)
     setRegalos(regalosNoBorrados)
   }
-   const handleDeleteAll = () =>{
+
+  const handleDeleteAll = () =>{
     setRegalos([])
   }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(nuevoRegalo !== ''){
+      if(editarRegaloId){
+        const regalosEditados = regalos.map(regalo => {
+          if(regalo.id === editarRegaloId){
+            return {...regalo, name: nuevoRegalo, cantidad: cantidadregalos, img: imagenRegalo, destinatario: destinatario}
+          }
+          return regalo
+        })
+        setRegalos(regalosEditados)
+      }else{
+        setRegalos([...regalos, {name: nuevoRegalo, id: regalos.length + 1, cantidad: cantidadregalos, img: imagenRegalo, destinatario: destinatario }])
+      }
+      setCantidadRegalos(0)
+      setNuevoRegalo('')
+      setDestinatario('')
+      setOpenModal(false)
+      setEditarRegaloId(null)
+    }
+  }
+
+  const handleModal = (regalo) => {
+    if(regalo){
+      setNuevoRegalo(regalo.name)
+      setCantidadRegalos(regalo.cantidad)
+      setImagenRegalo(regalo.img)
+      setDestinatario(regalo.destinatario)
+      setEditarRegaloId(regalo.id)
+    }
+    setOpenModal(true)
+  }
+
+  const closeModal =(e) =>{
+    setCantidadRegalos(0)
+    setNuevoRegalo('')
+    setDestinatario('')
+    setOpenModal(false)
+    setEditarRegaloId(null)
+  }
+
+  useEffect(()=>{
+    localStorage.setItem("regalos", JSON.stringify(regalos));
+  }, [regalos])
+
   return (
     <div className='container'>
       <h1>Regalos:</h1>
-      <div className='formulario'>
-        <input value={nuevoRegalo} onChange={(e)=>setNuevoRegalo(e.target.value)} type="text"/>
-        <button onClick={handleOnClick}>Agregar</button>
-      </div>
+     
+      <button onClick={() => handleModal()} className="agregarregalo">Agregar regalo</button>
+      <Modal
+        isOpen={openModal}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        className="modal"
+        overlayClassName="overlayModal"
+      >
+        <form onSubmit={handleSubmit} className='formulario'>
+          <input value={nuevoRegalo} placeholder='Nombre Regalo' onChange={(e) => setNuevoRegalo(e.target.value)} type="text"/>
+          <input value={imagenRegalo} placeholder='http:/image...' onChange={(e) => setImagenRegalo(e.target.value)} className='inputimage' type="text" ></input>
+          <div className='inputnumber'>
+            <input value={cantidadregalos} placeholder='cantidad' onChange={(e) => setCantidadRegalos(e.target.value)} type="number"></input>
+          </div>
+          <input value={destinatario} placeholder='Nombre destinatario' onChange={(e) => setDestinatario(e.target.value)} type="text"></input>
+          <div className='formactions'>
+          <button type="submit" className='formactionsbuttons'>{editarRegaloId ? 'Editar' : 'Agregar'}</button>
+          <button onClick={closeModal}className='formactionsbuttons'>Cerrar</button>
+          </div>
+        </form>
+      </Modal>
       {!regalos.length ? <span className="listavacia">No hay regalos grinch! agrega algo:)</span> :
       <div className="listaregalos" >
         <ul className="regalos">
         {regalos.map(regalo =>{
-          return <li className="itemregalo" key={regalo.id}><span>{regalo.name}</span>
+          return <li className="itemregalo" key={regalo.id}><img src={regalo.img} alt=""/><span>{regalo.name} {regalo.cantidad}</span><span>{regalo.destinatario}</span>
+                  <button className="editar" onClick={()=>handleModal(regalo)}>E</button>
                   <button className="delete" onClick={()=>handleDelete(regalo.id)}>X</button></li>
         })}
         </ul>
@@ -40,7 +112,6 @@ const [regalos, setRegalos] = useState([{name:'Caramelos', id: 0}, {name:'Medias
       <div className='borrartodo'>
         <button onClick={()=> handleDeleteAll()}>Borrar todo</button>
       </div>
-
     </div>
   );
 }
